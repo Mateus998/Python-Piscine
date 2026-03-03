e86a2599abf61563d66a6b1bf2534ca123a0bb4f
Mateus Ferreira Coelho Provetti Leite <mateferr@c2r4s4.42lisboa.com>
2026-03-03 18:08:55 +0000

    ex03_00

---
+++ Py00/ex03/count.py
@@ -0,0 +1,40 @@
+import string
+from collections import Counter
+from typing import Literal
+
+def classify(c: str) -> Literal["space", "puntc", "upper", "lower", "other"]:
+    if c == " ":
+        return "space"
+    elif c is string.punctuation:
+        return "puntc"
+    elif c.isupper():
+        return "upper"
+    elif c.islower():
+        return "lower"
+    else:
+        return "other"
+
+def text_analyser(text = None) -> None:
+    """
+    Takes a single string as argument and displays the sums of its upper-case characters, lower-case characters, punctuation characters and spaces.
+    If None or nothing is provided, the user is prompted to provide a string.
+    If the argument is not a string, print an error message.
+    """
+    if text is None:
+        text = input("Incert a string\n")
+    elif not isinstance(text, str):
+        print("String argument required")
+        return
+    
+    assert isinstance(text, str), "argument is not a string"
+
+    counts = Counter(classify(c) for c in text) #generator expression expressao for variavel in iteravel
+
+    # upper = sum(1 for c in text if c.isupper())
+    # upper = sum(c.isupper() for c in text)
+
+    print(f'The text contains {len(text)} character(s):')
+    print(f'- {counts["upper"]} upper letter(s)')
+    print(f'- {counts["lower"]} lower letter(s)')
+    print(f'- {counts["punct"]} punctuation mark(s)')
+    print(f'- {counts["space"]} space(s)')
