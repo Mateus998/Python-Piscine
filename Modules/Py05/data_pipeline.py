dd5381f6dc62f0acecbb8e5147b7d181d45f13df
mateus <mateus@example.com>
2026-07-04 21:17:00 +0100

    Py05 done

--- Modules/Py05/data_pipeline.py
+++ Modules/Py05/data_pipeline.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any
+from typing import Protocol
 import re
 
 type Number = int | float
@@ -98,20 +99,29 @@ class LogProcessor(DataProcessor):
         
         items: list[dict[str, str]] = data if isinstance(data, list) else [data]
 
-        for item in items:
-            self.data.append((self.index, str(item)))
+        # dictList = [f"{d['log_level']}: {d['log_message']}" for d in items]
+        dictList = [': '.join(d.values()) for d in items]
+
+        for item in dictList:
+            self.data.append((self.index, item))
             self.index += 1
+
+class ExportPlugin(Protocol):
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        pass
 
 class DataStream:
     def __init__(self) -> None:
         self.processors: list[DataProcessor] = []
 
     def register_processor(self, proc: DataProcessor) -> None:
-        print("\n== Registering processor ==")
+        name = re.sub(r'(?<!^)(?=[A-Z])', ' ', proc.__class__.__name__)
+        print(f"\n== Registering {name} ==")
         self.processors.append(proc)
 
     def process_stream(self, stream: list[Any]) -> None:
         print("\n== Processing stream ==")
+        print(f'Stream: {stream}')
         for element in stream:
             processor = next(
                 (p for p in self.processors if p.validate(element)), None
@@ -130,30 +140,66 @@ class DataStream:
             name = re.sub(r'(?<!^)(?=[A-Z])', ' ', p.__class__.__name__)
             print(f"{name}: total {p.index} items processed, remaining {len(p.data)} on processor")
 
+    def output_pipeline(self, nb: int, plugin: ExportPlugin) -> None:
+        name = plugin.__class__.__name__
+        print(f'\nSend {nb} processed data from each processor to a {name} plugin:')
+        for proc in self.processors:
+            outList: list[tuple[int, str]] = []
+            for _ in range(nb):
+                try:
+                    outList.append(proc.output())
+                except Exception:
+                    pass
+            plugin.process_output(outList)
+
+
+class CSV:
+    def __init__(self) -> None:
+        pass
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        print('CSV Output:')
+        print(', '.join(elem[1] for elem in data))
+
+class JSON:
+    def __init__(self) -> None:
+        pass
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        pairs = (f'\"item_{item[0]}\": \"{item[1]}\"' for item in data)
+        print('JSON Output:')
+        print('{' + ', '.join(pairs) + '}')
+
 def main():
     boss = DataStream()
     np = NumericProcessor()
     tp = TextProcessor()
     lp = LogProcessor()
     stream: list[Any] = ['Hello world', [3.14, -1, 2.71], [{'log_level': 'WARNING', 'log_message': 'Telnet access! Use ssh instead'}, {'log_level': 'INFO', 'log_message': 'User wil is connected'}], 42, ['Hi', 'five']]
+    stream2: list[Any] =  [21, ['I love AI', 'LLMs are wonderful', 'Stay healthy'], [{'log_level': 'ERROR', 'log_message': '500 server crash'}, {'log_level': 'NOTICE', 'log_message': 'Certificate expires in 10 days'}], [32, 42, 64, 84, 128, 168], 'World hello']
+    csv = CSV()
+    json = JSON()
 
     boss.print_processors_stats()
 
     boss.register_processor(np)
-
-    boss.process_stream(stream)
-
-    boss.print_processors_stats()
-
-    print("\nConsume 4 Numeric Processor items")
-    [np.output() for _ in range(4)]
-
     boss.register_processor(tp)
     boss.register_processor(lp)
 
     boss.process_stream(stream)
 
     boss.print_processors_stats()
+
+    boss.output_pipeline(3, csv)
+
+    boss.print_processors_stats()
+
+    boss.process_stream(stream2)
+
+    boss.print_processors_stats()
+
+    boss.output_pipeline(5, json)
+
+    boss.print_processors_stats()
+
 
 if __name__ == "__main__":
     main()
